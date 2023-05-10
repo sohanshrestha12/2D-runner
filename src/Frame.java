de851885ebd871af8eb1d0d4dce04b448ec67c39
sohanshrestha12 <sohanshrestha40@gmail.com>
2023-05-10 22:53:44 +0545

    merged

--- src/Frame.java
+++ src/Frame.java
@@ -3,13 +3,24 @@ import java.awt.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Random;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 
 public class Frame extends mainHero{
     private int xOffset = 0;
     private int oxOffset = 1100;
     public JPanel startScreen;
+    public int oy1 =(int) (Math.random() * 2) * 100;
+    public int oy2 = (int) (Math.random() * 2) * 100;
+    public Rectangle obstacle1Rec;
+    public Rectangle obstacle2Rec;
+    public Rectangle obstacle3Rec;
+    public Rectangle obstacle4Rec;
+    public Rectangle obstacle5Rec;
+    public Timer timer1;
+    JPanel GameOver;
+    JLayeredPane mainWindow;
     mainHero character=new mainHero();
     public JFrame frame = new JFrame("Runner");
     private JButton startButton,exitButton;
@@ -37,7 +48,7 @@ public class Frame extends mainHero{
                 super.paintComponent(g);
                 // load the image file
 //                E:\sohan\Java\JavaProjectM\2D-Runner\Game\Assets
-                Image backgroundImage = new ImageIcon("E:\\JavaGame\\2D-Runner\\Game\\Assets\\ICON.png").getImage();
+                Image backgroundImage = new ImageIcon("E:\\Game\\Assets\\ICON.png").getImage();
                 g.drawImage(backgroundImage,0,0,Color.WHITE,null);
             }
         };
@@ -52,7 +63,7 @@ public class Frame extends mainHero{
                 super.paintComponent(g);
                 // load the image file
 //                E:\sohan\Java\JavaProjectM\2D-Runner\Game\Assets
-                Image backgroundImage = new ImageIcon("E:\\JavaGame\\2D-Runner\\Game\\Assets\\10.png").getImage();
+                Image backgroundImage = new ImageIcon("E:\\Game\\Assets\\10.png").getImage();
                 xOffset--;
                 if (xOffset < -frame.getWidth()) {
                     xOffset = 0;
@@ -63,8 +74,8 @@ public class Frame extends mainHero{
         };
         gameWindow.setPreferredSize(new Dimension(1100, 700));
         gameWindow.setBounds(0, 0, 1100, 700);
-        Timer timer1 = new Timer(100, e -> gameWindow.repaint());
-        timer1.start();
+        Timer timer0 = new Timer(100, e -> gameWindow.repaint());
+        timer0.start();
         //Start Button
         startButton = new JButton("Start");
         startButton.setBounds(500,240,100,50);
@@ -113,7 +124,7 @@ public class Frame extends mainHero{
         frame.setLocationRelativeTo(null);*/
         frame.getContentPane().removeAll();
 
-        JLayeredPane mainWindow = new JLayeredPane();
+        mainWindow = new JLayeredPane();
         mainWindow.setBounds(0,0,1100,700);
 
         // background
@@ -123,7 +134,7 @@ public class Frame extends mainHero{
                 super.paintComponent(g);
                 // load the image file
 //                E:\sohan\Java\JavaProjectM\2D-Runner\Game\Assets
-                Image backgroundImage = new ImageIcon("E:\\JavaGame\\2D-Runner\\Game\\Assets\\10.png").getImage();
+                Image backgroundImage = new ImageIcon("E:\\Game\\Assets\\10.png").getImage();
                 int height = frame.getHeight() - 250;
                 xOffset--;
                 if (xOffset < -frame.getWidth()) {
@@ -144,7 +155,7 @@ public class Frame extends mainHero{
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 // load the image file
-                Image backgroundRoad = new ImageIcon("E:\\JavaGame\\2D-Runner\\Game\\Assets\\Road.png").getImage();
+                Image backgroundRoad = new ImageIcon("E:\\Game\\Assets\\Road.png").getImage();
                 int height = frame.getHeight() / 3;
 //                System.out.println(gameWindow.getHeight());
                 xOffset--;
@@ -163,67 +174,62 @@ public class Frame extends mainHero{
         mainWindow.add(mainChar, Integer.valueOf(2));
 
         //obstacle
-
-
-
         JPanel obstacle1 = new JPanel(){
+
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
 
-                Image obstaclebackground = new ImageIcon("E:\\JavaGame\\2D-Runner\\Game\\Assets\\car1.png").getImage();
+                Image obstaclebackground = new ImageIcon("E:\\Game\\Assets\\car1.png").getImage();
 
                 int height = 100;
-                oxOffset-=5;
-//                System.out.println(oxOffset);
+                oxOffset -= 5;
+
                 if (oxOffset < -frame.getWidth()-100) {
                     oxOffset = frame.getWidth();
+                    oy1 = (int) (Math.random() * 2) * 100;
                 }
-                Rectangle obstacle1=new Rectangle(oxOffset, 0, 100, height);
-                Rectangle obstacle2=new Rectangle(oxOffset + 800, 100, 100, height);
-                Rectangle hero=character.mainherorec;
-                g.drawImage(obstaclebackground, oxOffset, 0, 150, height, null);
-                g.drawImage(obstaclebackground, oxOffset + 800, 100, 150, height, null);
-                if(obstacle1.intersects(hero)||obstacle2.intersects(hero))
-                {
-                    System.out.println("Collision");
-                    frame.getContentPane().removeAll();
-                    StartScreen();
-                }
+//                System.out.println(oy1);
+                obstacle2Rec = new Rectangle(oxOffset , oy1, 100, height);
+                obstacle1Rec = new Rectangle(oxOffset + 800, oy1, 100, height);
+//                g.setColor(Color.BLUE);
+//                g.fillRect(obstacle1Rec.x, obstacle1Rec.y, obstacle1Rec.width, obstacle1Rec.height);
+//                g.fillRect(obstacle2Rec.x, obstacle2Rec.y, obstacle2Rec.width, obstacle2Rec.height);
 
-
-
+                g.drawImage(obstaclebackground, oxOffset, oy1, 150, height, null);
+                g.drawImage(obstaclebackground, oxOffset+800, oy1, 150, height, null);
             }
         };
         obstacle1.setOpaque(false);
         obstacle1.setBounds(0, 445, 1100, 220);
         mainWindow.add(obstacle1, Integer.valueOf(2));
+
+
         JPanel obstacle2 = new JPanel(){
+
+
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
 
-                Image obstaclebackground = new ImageIcon("E:\\JavaGame\\2D-Runner\\Game\\Assets\\car2.png").getImage();
+                Image obstaclebackground = new ImageIcon("E:\\sohan\\Java\\JavaProjectM\\2D-Runner\\Game\\Assets\\car2.png").getImage();
                 int height = 100;
-                oxOffset-=5;
-//                System.out.println(oxOffset);
+                oxOffset -= 5;
+
                 if (oxOffset < -frame.getWidth()-100) {
                     oxOffset = frame.getWidth();
+                    oy2 = (int) (Math.random() * 2) * 100;
                 }
-                Rectangle obstacle1=new Rectangle(oxOffset+270, 100, 100, height);
-                Rectangle obstacle2=new Rectangle(oxOffset + 550, 0, 100, height);
-                Rectangle obstacle3=new Rectangle(oxOffset + 1020, 0, 100, height);
-                Rectangle hero=character.mainherorec;
-                g.drawImage(obstaclebackground, oxOffset+270, 100, 150, height, null);
-                g.drawImage(obstaclebackground, oxOffset + 550, 0, 150, height, null);
-                g.drawImage(obstaclebackground, oxOffset + 1020, 0, 150, height, null);
-                if(obstacle1.intersects(hero)||obstacle2.intersects(hero)||obstacle3.intersects(hero))
-                {
-                    System.out.println("Collision");
-                    frame.getContentPane().removeAll();
-                    StartScreen();
-                }
+                obstacle3Rec = new Rectangle(oxOffset + 270, oy2, 100, height);
+                obstacle4Rec = new Rectangle(oxOffset + 550, oy2+100, 100, height);
+                obstacle5Rec = new Rectangle(oxOffset + 1040, oy2, 100, height);
+                g.setColor(Color.BLUE);
+                g.fillRect(obstacle3Rec.x, obstacle3Rec.y, obstacle3Rec.width, obstacle3Rec.height);
+                g.fillRect(obstacle4Rec.x, obstacle4Rec.y, obstacle4Rec.width, obstacle4Rec.height);
 
+                g.drawImage(obstaclebackground, oxOffset + 270, oy2, 150, height, null);
+                g.drawImage(obstaclebackground, oxOffset + 550, oy2+100, 150, height, null);
+                g.drawImage(obstaclebackground, oxOffset + 1040, oy2, 150, height, null);
             }
         };
         obstacle2.setOpaque(false);
@@ -231,20 +237,68 @@ public class Frame extends mainHero{
         mainWindow.add(obstacle2, Integer.valueOf(2));
 
         frame.getContentPane().add(mainWindow);
-        Timer timer1 = new Timer(100, e -> gameWindow.repaint());
+        timer1 = new Timer(100, e -> {
+//            System.out.println("mh: " + character.y);
+            if ((character.mainherorec.intersects(obstacle1Rec))||(character.mainherorec.intersects(obstacle2Rec))||(character.mainherorec.intersects(obstacle3Rec))||(character.mainherorec.intersects(obstacle4Rec))||(character.mainherorec.intersects(obstacle5Rec))) {
+//                System.out.println("collision occured");
+                handleCollision();
+            }
+            gameWindow.repaint();
+            gameRoad.repaint();
+
+        });
         timer1.start();
-        Timer timer2 = new Timer(100, e -> gameRoad.repaint());
-        timer2.start();
+        GameOver = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
 
-        Timer timer3 = new Timer(150, e -> obstacle1.repaint());
-        timer3.start();
+                // Display "Game Over" message
+                Font gameOverFont = new Font("Arial", Font.BOLD, 90);
+                Font restartFont = new Font("Arial", Font.PLAIN, 40);
+                g.setColor(Color.white);
+                g.setFont(gameOverFont);
+                g.drawString("Game Over!", 300, 200);
 
-        Timer timer4 = new Timer(150, e -> obstacle2.repaint());
-        timer4.start();
+                g.setFont(restartFont);
+                g.drawString("Press Space to Restart", 350, 270);
+            }
+
+        };
+        GameOver.setBounds(0, 0, 1100, 700);
+        GameOver.setFocusable(true);
+//        GameOver.setBackground(Color.BLUE);
+        GameOver.setVisible(false);
+        GameOver.setOpaque(false);
+        mainWindow.add(GameOver, Integer.valueOf(3));
+        mainWindow.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (GameOver.isVisible() && e.getKeyCode() == KeyEvent.VK_SPACE) {
+//                    System.out.println("good job");
+                    GameOver.setVisible(false);
+                    timer1.start();
+                    character.characterstart();
+                    oxOffset = 1100;
+                    character.y=0;
+                    oy1 =(int) (Math.random() * 2) * 100;
+                    oy2 = (int) (Math.random() * 2) * 100;
+                    character.gameCharacter.requestFocusInWindow();
+                }
+            }
+        });
+
+
 
         mainChar.setFocusable(true);
         mainChar.requestFocusInWindow();
         frame.setVisible(true);
+    }
+    void handleCollision() {
+        timer1.stop();
+        character.characterstop();
+        mainWindow.requestFocusInWindow();
+        GameOver.setVisible(true);
     }
 
 }
